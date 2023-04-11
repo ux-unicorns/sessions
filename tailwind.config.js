785c63b5afe40b0a738334a01597cd140970b9eb
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-04-11 12:30:21 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}',
+  './components/**/*.{html,js}',
+  "./public/**/*.{html,js}",],
+  theme: {
+    extend: {colors: {
+      primary: '#00D285', 
+      secondary: {
+        100: '#E2E2D5',
+        200: '#888883'
+      }
+    },
+    fontFamily: {
+      body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
+

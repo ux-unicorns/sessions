049b29e95ee697f91c0b838b363fc7fc16e6611a
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-04-12 17:48:52 +0400

    worked on the homepage

--- module/menu.js
+++ module/menu.js
@@ -1,16 +1,18 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 const discover = document.querySelector('.intro');
-const herosection = 
+// const intro = document.querySelector('.intro-picture');
 
 burger.addEventListener('click', () =>{
     if(menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
         menu.classList.add('flex');
         discover && discover.classList.add("space");
+        intro && (intro.style.top = "10em");
     } else{
         menu.classList.remove('flex');
         menu.classList.add('hidden');
         discover && discover.classList.remove("space");
+        intro && (intro.style.top = "0em");
     }
 });
